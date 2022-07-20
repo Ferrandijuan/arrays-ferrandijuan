9110de2bd9591604ab2aa2dc4e56ca7fa8ac2c53
ferrandijuan <104110179+Ferrandijuan@users.noreply.github.com>
2022-07-20 11:18:48 -0300

    eventos

--- main.js
+++ main.js
@@ -1,146 +1,92 @@
-let opcion = 0;
-let flag = 0;
-let producto;
-let gbmemoria;
-let precio = 0;
-let totalcompra= 0;
-let i=0;
-
-class celular{
-    constructor(modelo, memoria) {
-        this.modelo = modelo;
-        this.memoria = memoria;
-    }
-}
-function AsignacionProducto (producto){
-    switch (producto){
-        case "1":
-            producto= "IPhone 12 Pro Max";
-            break
-        case "2":
-            producto= "Samsung Galaxy S22";
-            break
-        case "3":
-            producto= "Moto G Stylus";
-            break
-        case "4":
-            producto= "Xiaomi Redmi note 11";
-            break
-        case "5":
-            producto= "Huawei P40 pro";
-            break
-    }
-    return producto;
-}
-function AsignacionMemoria (memoria){
-    if(memoria == 1){
-        memoria = "128 GB"
-    }
-    else{
-        memoria = "256 GB";
-    }
-    return memoria;
-}
-function cargaobjeto (objeto, arreglo){
-  flag=0;
-    do {
-        producto = prompt(
-          "Buenos dias, que celular estabas buscando? \n 1)iPhone 12 Pro Max: $820 \n 2)Samsung Galaxy S22: $690 \n 3)Moto G Stylus: $320 \n 4)Xiaomi Redmi note 11: $410 \n 5)Huawei P40 pro: $800 "
-        );
-
-        if (producto == "1" || producto == "2" || producto == "3" || producto == "4" || producto == "5") {
-          alert(
-            "Agregado al carrito"
-          )
-          flag = 1;
-        } else {
-          alert("Numero incorrecto");
-        }
-
-    } while (flag == 0);
-    flag=0;
-    do{
-        gbmemoria = prompt("De cuantos GB estaba buscando? \n 1)128GB \n 2)256GB(+130USD)");
-        if (gbmemoria == "1" || gbmemoria == "2") 
-        {
-            alert
-            (
-              "agregado al carrito"
-            )
-            flag = 1;
-        }
-        else
-        {
-        alert("Numero incorrecto");
-        }
-    }while(flag == 0);
-    flag=0;
-    totalcompra=calculadoraDePrecios(producto,gbmemoria); 
-    
-    producto=AsignacionProducto(producto);
-    objeto.modelo=producto;
-    
-    gbmemoria=AsignacionMemoria(gbmemoria);
-    objeto.memoria=gbmemoria; 
-    celulares[i]= objeto.modelo+ " "+objeto.memoria;  
-    i++;                                               
-
-}
-function calculadoraDePrecios (modelo, memoria){
-  switch (modelo){
-    case "1": precio += 820;
-    break;
-
-    case "2": precio += 690;
-    break;
-
-    case "3": precio += 320;
-    break;
-
-    case "4": precio += 410;
-    break;
-
-    case "5": precio += 800;
-    break;
-    default:
-      return 0;
-      break;
-  }
-
-  if (memoria == "2") {
-    precio += 130
+const carrito = document.querySelector("#carrito");
+const contenedor = document.querySelector("#contenedor");
+const vaciarCarrito = document.querySelector("#vaciar-carrito");
+const listaTelefonos = document.querySelector("#lista-telefonos");
+let celularesCarrito = [];
 
 
-}
-return precio;
+cargarEventListeners();
 
+function cargarEventListeners() {
+  listaTelefonos.addEventListener("click", agregarCelular);
+
+  carrito.addEventListener("click", eliminarCelular);
+
+
+
+  vaciarCarrito.addEventListener("click", () => {
+      celularesCarrito = [];
+
+    limpiarHTML();
+  });
 }
 
-let celulares = []; 
 
-do {
-  cargaobjeto(celular,celulares);
-    do{
-    opcion = prompt("Desea seguir comprando? \n 1)seguir comprando \n 2)finalizar compra")
-      ;
-      if (opcion == "1" || opcion == "2") {
-      flag = 1;
-    } else {
-      alert("numero incorrecto");
-    }
-    } while (flag == 0);
+function agregarCelular(e) {
+    e.preventDefault();
   
-  } while (opcion == 1);
-  alert("total de compra: "+ totalcompra);
-  for(let i=0;i<celulares.length;i++){
-    alert("USTED COMPRÓ\n"+celulares[i]);
+    if (e.target.classList.contains("agregar-celular")) {
+      const celularSeleccionado = e.target.parentElement;
+      DatosCelulares(celularSeleccionado);
+    }
+  }
+
+  function DatosCelulares(celular) {
+    const infoCelulares = {
+      imagen: celular.querySelector("img").src,
+      titulo: celular.querySelector(".card-title").innerText,
+      precio: celular.querySelector("h6").innerText,
+      id: celular.querySelector("a").getAttribute("id"),
+      cantidad: 1,
+    };
+ 
+  
+    const existe = celularesCarrito.some((celular) => celular.id === infoCelulares.id);
+    if (existe) {
+      const celulares = celularesCarrito.map((celular) => {
+        if (celular.id === infoCelulares.id) {
+          celular.cantidad++;
+          return celular; 
+        } else {
+          return celular; 
+        }
+      });
+      celularesCarrito = [...celulares];
+    } else {
+      celularesCarrito = [...celularesCarrito, infoCelulares];
+    }
+    carritoHTML();
   }
 
 
-celulares.sort() //Ordenados por orden alfabetico
-  for (let i=0;i<celulares.length;i++){
-    console.log(celulares[i]);
+  function carritoHTML() {
+    limpiarHTML();
+
+    celularesCarrito.forEach((celular) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>${celular.titulo}</td>
+      <td>${celular.precio}</td>
+      <td>${celular.cantidad}</td>
+      <td>
+      <a href="" class="borrar-celular" id="${celular.id}">x</a>
+      </td>`;
+      contenedor.appendChild(row);
+    });
   }
 
-let telefonosDe256gb = celulares.filter(elemento => elemento.includes("256 GB"));
-  console.log(telefonosDe256gb);
+function eliminarCelular(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("borrar-celular")) {
+      const celularID = e.target.getAttribute("id");
+
+      celularesCarrito = celularesCarrito.filter((celular) => celular.id !== celularID);
+  
+      carritoHTML();
+    }
+  }
+
+  
+function limpiarHTML() {
+    contenedor.innerHTML = "";
+  }
+
